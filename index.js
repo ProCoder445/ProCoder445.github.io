2403a2c9101eb979c6a906d8b9ba0287ea785e81
Caleb <cszachariah@gmail.com>
2025-05-01 18:15:02 -0400

    Updated game, fixed jumping

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const context = canvas.getContext("2d");
 
 let position = {
 	x: 0,
-	y: 0
+	y: canvas.height - 100
 }
 
 let size = {
@@ -22,20 +22,28 @@ let keyPressed = false;
 
 
 
-let gravity = 9.78;
+let gravity = 0.5;
+
+function jump() {
+	
+	position.y += acceleration.y;
+
+
+	if(position.y + size.height >= canvas.height) {
+		acceleration.y = 0;
+		position.y = canvas.height - size.height;;
+	} else acceleration.y += gravity
+	
+
+}
 
 function animate() {
 	window.requestAnimationFrame(animate);
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	context.fillRect(position.x, position.y, size.width, size.height);
+	jump();
 
-	if(position.y + size.height < canvas.height && !keyPressed) position.y += gravity;
-
-	else if(position.y + size.height >= canvas.height) position.y -= acceleration.y;
-
-	position.x += acceleration.x	
-	
-	
+	position.x += acceleration.x;
 
 
 }
@@ -45,21 +53,59 @@ function animate() {
 window.addEventListener("keydown", (event) => {
 	console.log(event.key);
 	switch(event.key) {
-	case "ArrowUp" || "w":
-		acceleration.y = 30;
-		keyPressed = true;
-		break;
-
-	case "ArrowLeft" || "a":
+	case "ArrowLeft":
 		acceleration.x = -6;
 		break;
 
-	case "ArrowRight" || "d":
+	case "ArrowRight":
 		acceleration.x = 6
 		break; 
 	}
 
 	console.log(acceleration);
+});
+
+
+window.addEventListener("keydown", (event) => {
+	switch(event.key) {
+	case "ArrowUp":
+		acceleration.y = -20;
+		keyPressed = true;
+		break;
+
+	}
+
+});
+
+
+
+
+window.addEventListener("keyup", (event) => {
+	console.log(event.key);
+	switch(event.key) {
+
+	case "ArrowLeft":
+		acceleration.x = 0;
+		break;
+
+	case "ArrowRight":
+		acceleration.x = 0;
+		break; 
+	}
+
+	console.log(acceleration);
+});
+
+
+
+
+window.addEventListener("keyup", (event) => {
+	console.log(event.key);
+	switch(event.key) {
+
+	case "ArrowUp":
+		keyPressed = false;
+	}
 });
 
 
